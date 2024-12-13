063fbe592a8c5b9855d97f01eeeecf5065be4722
Kirill <kirillsavorin@gmail.com>
2024-12-14 00:26:26 +0100

    Menu, Rating, Challenge class

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+import Challenge from "./Challenge";
+import Menu from "./Menu";
+
+const menu = new Menu();
+menu.add();
+
+for (let x = 0; x < 3; x++) {
+const challenge = new Challenge({
+    title: 'Challenge: ' + x,
+    description: "Hello, this is description",
+    type: 'online',
+    minParticipants: 2,
+    maxParticipants: 5,
+    rating: 4,
+    image: "https://placecats.com/600/400",
+    tags: ['foo', 'bar']
+});
+
+const elem = challenge.render(document);
+
+document.querySelector(".popular-rooms").append(elem);
+}
