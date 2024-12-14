85dc0f29b5b8b69f151d69a266760e7a21a68ddf
Kirill <kirillsavorin@gmail.com>
2024-12-14 12:33:50 +0100

    API backend

---
+++ src/js/Challenge.js
@@ -0,0 +1,40 @@
+import Rating from "./Rating";
+
+export default class Challenge {
+    constructor(data) {
+        this.data = data;
+    }
+
+    render(elemCreator) {
+        const container = elemCreator.createElement('article');
+        container.className = "room";
+
+        const img = elemCreator.createElement('img');
+        img.src = this.data.image;
+        container.append(img);
+
+        const h2 = elemCreator.createElement('h2');
+        h2.textContent = this.data.title;
+        container.append(h2);
+
+        const rating = new Rating(this.data.rating, "challengeRating");
+        const ratingElem = rating.render(elemCreator);
+        container.append(ratingElem);
+
+        const groupSize = elemCreator.createElement("span");
+        groupSize.className = "participants";
+        groupSize.textContent = `${this.data.minParticipants}-${this.data.maxParticipants} participants`;
+        container.append(groupSize);
+
+        const description = elemCreator.createElement("p");
+        description.textContent = this.data.description;
+        container.append(description)
+
+        const button = elemCreator.createElement("button");
+        button.className = "red-button";
+        button.textContent = this.data.type == 'online' ? 'Take challenge online' : "Book this room";
+        container.append(button);
+
+        return container;
+    }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,16 @@
+import ApiBackend from "./ApiBAckend";
 import Challenge from "./Challenge";
 import Menu from "./Menu";
 
 const menu = new Menu();
 menu.add();
 
+const backend = new ApiBackend("https://lernia-sjj-assignments.vercel.app/api");
+
+const challenges = await backend.loadAllChallenges();
+console.log(challenges);
 for (let x = 0; x < 3; x++) {
-const challenge = new Challenge({
-    title: 'Challenge: ' + x,
-    description: "Hello, this is description",
-    type: 'online',
-    minParticipants: 2,
-    maxParticipants: 5,
-    rating: 2,
-    image: "https://placecats.com/600/400",
-    tags: ['foo', 'bar']
-});
+const challenge = new Challenge(challenges[x]);
 
 const elem = challenge.render(document);
 
