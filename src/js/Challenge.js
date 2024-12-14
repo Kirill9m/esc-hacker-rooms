06396bc72fa23ee2cd218872f14348a7529ee882
Kirill9m <127823714+Kirill9m@users.noreply.github.com>
2024-12-14 19:56:13 +0000

    idk

--- src/js/Challenge.js
+++ src/js/Challenge.js
@@ -6,38 +6,36 @@ export default class Challenge {
     }
 
     render(elemCreator) {
-        const container = elemCreator.createElement('article');
-        container.className = "room";
+        this.container = elemCreator.createElement('article');
+        this.container.className = "room";
 
         const img = elemCreator.createElement('img');
         img.src = this.data.image;
-        container.append(img);
+        this.container.append(img);
 
         const h2 = elemCreator.createElement('h2');
         h2.textContent = this.data.title;
-        container.append(h2);
+        this.container.append(h2);
 
         const rating = new Rating(this.data.rating, "challengeRating");
         const ratingElem = rating.render(elemCreator);
-        container.append(ratingElem);
+        this.container.append(ratingElem);
 
         const groupSize = elemCreator.createElement("span");
         groupSize.className = "participants";
         groupSize.textContent = `${this.data.minParticipants}-${this.data.maxParticipants} participants`;
-        container.append(groupSize);
+        this.container.append(groupSize);
 
         const description = elemCreator.createElement("p");
         description.textContent = this.data.description;
-        container.append(description)
+        this.container.append(description)
 
         const button = elemCreator.createElement("button");
         button.className = "red-button";
         button.textContent = this.data.type == 'online' ? 'Take challenge online' : "Book this room";
-        container.append(button);
-
-        this.container = container;
+        this.container.append(button);
         
-        return container;
+        return this.container;
     }
 
     toggleVisibility(visible) {
